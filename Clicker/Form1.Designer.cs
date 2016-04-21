d50897034e4a4738e3a5dc8833a46b9840235590
mastermind <cyberdude@mail.ru>
2016-04-21 15:38:52 +0300

    +

---
+++ Clicker/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Numeric_Clicks_Delay = new System.Windows.Forms.NumericUpDown();
+            this.Numeric_Clicks = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.Numeric_Clicks_Delay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Numeric_Clicks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(55, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Q";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Клавиша, по по которой сработает клик:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Задержка между кликами [мс]:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Кликов за нажатие:";
+            // 
+            // Numeric_Clicks_Delay
+            // 
+            this.Numeric_Clicks_Delay.Location = new System.Drawing.Point(185, 91);
+            this.Numeric_Clicks_Delay.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.Numeric_Clicks_Delay.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Numeric_Clicks_Delay.Name = "Numeric_Clicks_Delay";
+            this.Numeric_Clicks_Delay.Size = new System.Drawing.Size(51, 20);
+            this.Numeric_Clicks_Delay.TabIndex = 3;
+            this.Numeric_Clicks_Delay.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Numeric_Clicks_Delay.ValueChanged += new System.EventHandler(this.Numeric_Clicks_Delay_ValueChanged);
+            // 
+            // Numeric_Clicks
+            // 
+            this.Numeric_Clicks.Location = new System.Drawing.Point(126, 62);
+            this.Numeric_Clicks.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Numeric_Clicks.Name = "Numeric_Clicks";
+            this.Numeric_Clicks.Size = new System.Drawing.Size(53, 20);
+            this.Numeric_Clicks.TabIndex = 3;
+            this.Numeric_Clicks.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Numeric_Clicks.ValueChanged += new System.EventHandler(this.Numeric_Clicks_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 120);
+            this.Controls.Add(this.Numeric_Clicks);
+            this.Controls.Add(this.Numeric_Clicks_Delay);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clicker";
+            ((System.ComponentModel.ISupportInitialize)(this.Numeric_Clicks_Delay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Numeric_Clicks)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown Numeric_Clicks_Delay;
+        private System.Windows.Forms.NumericUpDown Numeric_Clicks;
+    }
+}
+
---
+++ Clicker/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Clicker
+{
+    public partial class Form1 : Form
+    {
+        //[DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        //public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        static string interceptedKey = "";
+        static int clicksPerKey = 1;
+        static int clicks_Delay = 1;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _hookID = SetHook(_proc);
+            //Application.Run();
+            Application.Run(this);
+            
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        public static void DoMouseClick()
+        {
+            //Call the imported function with the cursor's current position
+            int X = Cursor.Position.X;
+            int Y = Cursor.Position.Y;
+            //var test = numer
+            for (int i = 0; i < clicksPerKey; i++)
+            {
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+                Thread.Sleep(clicks_Delay);
+            }
+                
+        }
+
+        //...other code needed for the application
+
+
+
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        //public static void Main()
+        //{
+        //    _hookID = SetHook(_proc);
+        //    Application.Run();
+        //    UnhookWindowsHookEx(_hookID);
+        //}
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(
+            int nCode, IntPtr wParam, IntPtr lParam);
+
+
+
+
+
+        
+        private static IntPtr HookCallback(
+            int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                interceptedKey = ((Keys)vkCode).ToString();
+
+                if (interceptedKey.ToLower() == interceptedKey.ToLower())
+                {
+                    //for (int i = 0; i < 5;i++ )
+                    //{
+                    //    DoMouseClick();
+                    //    //Thread.Sleep(30);
+                    //}
+                        
+                    DoMouseClick();
+                    //DoMouseClick();
+                    //DoMouseClick();
+                }
+                    
+                //Console.WriteLine((Keys)vkCode);
+                
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+
+
+
+
+        private void Numeric_Clicks_ValueChanged(object sender, EventArgs e)
+        {
+            clicksPerKey = (int)Numeric_Clicks.Value;
+        }
+
+        private void Numeric_Clicks_Delay_ValueChanged(object sender, EventArgs e)
+        {
+            clicks_Delay = (int)Numeric_Clicks_Delay.Value;
+        }
+    }
+}
